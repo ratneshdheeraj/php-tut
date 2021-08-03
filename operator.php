e198a0821543c374a28bd376835d4155c2eed15d
Ratnesh <parallelratnesh@gmail.com>
2021-08-03 20:30:35 +0545

    ope

---
+++ operator.php
@@ -0,0 +1,24 @@
+<?php
+
+$var1=100;
+$var2=200;
+
+echo $var1+$var2;
+
+print($var1+$var2);
+
+// - , * , /, %
+// ++, --
+// > ,<, <=, >=, ==, !=
+
+if($var1 > $var2){
+ echo "Yes";
+}else{
+    echo "No";
+}
+
+
+
+// (condition)? if statement : else statement;
+
+echo (100>50) ? "Hello" :"World";
